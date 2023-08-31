e3b850ba50da2fc6def7cad317f2f68236f12981
Tomas Peranic <peranic.tomas@hotmail.com>
2023-08-31 18:44:15 -0400

    Initial Commit

---
+++ ValU/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ValU
+//
+//  Created by Tomas Peranic on 2023-08-31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ValU/ValUApp.swift
@@ -0,0 +1,17 @@
+//
+//  ValUApp.swift
+//  ValU
+//
+//  Created by Tomas Peranic on 2023-08-31.
+//
+
+import SwiftUI
+
+@main
+struct ValUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
